68f0a90787d05ef00b9a3d84c4d88b4b95c39c49
Martin Rønning <martin@ronning.tv>
2023-06-05 22:32:13 +0200

    Dette er Martin og Mathias sitt forsøk på en image recon med svar.

--- JS/image-recon.js
+++ JS/image-recon.js
@@ -66,7 +66,9 @@ var imageList = [
 	"WavesAgainstTheShore-crop",
 	"WavesAgainstTheShore",
 ];
-imageListCropped = [
+
+/*
+var imageListCropped = [
 	"AndreasMunchStudyingAnatomy-crop",
 	"BathingGirls-crop",
 	"BathingMan-crop",
@@ -95,7 +97,7 @@ imageListCropped = [
 	"WavesAgainstTheShore-crop",
 ];
 
-imageListNormal = [
+var imageListNormal = [
 	"AndreasMunchStudyingAnatomy",
 	"BathingGirls",
 	"BathingMan",
@@ -107,22 +109,12 @@ imageListNormal = [
 	"FivePuppiesOnTheCarpet",
 	"ForestInSnow",
 	"FromMaridalen",
-	"HenrikIbsen",
-	"image1",
-	"image1_duplicate",
-	"image2",
-	"image2_duplicate",
-	"image3",
-	"image3_duplicate",
 	"InTheKennel",
 	"JealousyInTheGarden",
 	"KarenBjølstadInTheRockingChair",
 	"KarlJohanintheRain",
 	"LifeLeftPart",
 	"RecliningNudeonTheRocks",
-	"RolfHansen",
-	"SelfPortrait",
-	"StarryNight",
 	"TheFight",
 	"TheGirlFromNordland",
 	"TheGirlsOnTheBridge",
@@ -133,60 +125,141 @@ imageListNormal = [
 	"UninvitedGuests",
 	"WavesAgainstTheShore",
 ];
+*/
+var floor1List = [
+	"AndreasMunchStudyingAnatomy",
+	"BathingGirls",
+	"BathingMan",
+	"BathingMenOnRocks",
+	"BricklayerAtWork",
+];
 
-var imagePath = "../../Images/"; // Modify this path according to your directory structure
-var jpg = ".jpg";
-var fullImagePath = imageList.map(function (imageName) {
-	return imagePath + imageName + jpg;
-});
+var floor2List = [
+	"ChildrenandDucks",
+	"ConsolationInTheForest",
+	"DiggingMenWithHorseAndCart",
+	"FivePuppiesOnTheCarpet",
+	"ForestInSnow",
+];
+
+var floor3List = [
+	"FromMaridalen",
+	"InTheKennel",
+	"JealousyInTheGarden",
+	"KarenBjølstadInTheRockingChair",
+	"KarlJohanintheRain",
+];
+
+var floor4List = [
+	"LifeLeftPart",
+	"RecliningNudeonTheRocks",
+	"TheFight",
+	"TheGirlFromNordland",
+	"TheGirlsOnTheBridge",
+];
+
+var floor5List = [
+	"TheMurderer",
+	"TreesByTheSea",
+	"TwoWomenByTheVerandaSteps",
+	"TwoWomenInTheGarden",
+	"UninvitedGuests",
+	"WavesAgainstTheShore",
+];
+
+var floor1ListCrop = [
+	"AndreasMunchStudyingAnatomy-crop",
+	"BathingGirls-crop",
+	"BathingMan-crop",
+	"BathingMenOnRocks-crop",
+	"BricklayerAtWork-crop",
+];
+
+var floor2ListCrop = [
+	"ChildrenandDucks-crop",
+	"ConsolationInTheForest-crop",
+	"DiggingMenWithHorseAndCart-crop",
+	"FivePuppiesOnThecarpet-crop",
+	"ForestInSnow-crop",
+];
+
+var floor3ListCrop = [
+	"FromMaridalen-crop",
+	"inTheKennel-crop",
+	"JealousyInTheGarden-crop",
+	"KarenBjølstadInTheRockingChair-crop",
+	"KarlJohanintheRain-crop",
+];
+
+var floor4ListCrop = [
+	"LifeLeftPart-crop",
+	"RecliningNudeOnTheRocks-crop",
+	"TheFight-crop",
+	"TheGirlFromNordland-crop",
+	"TheGirlsOnTheBridge-crop",
+];
+
+var floor5ListCrop = [
+	"TheMurderer-crop",
+	"TreesByTheSea-crop",
+	"TwoWomenByTheVerandaSteps-crop",
+	"TwoWomenIntheGarden-crop",
+	"UninvitedGuests-crop",
+	"WavesAgainstTheShore-crop",
+];
+
+var currentFloorArray;
+var currentImage = "there is no current image selected"; //letting the user know that there is no current image selected
+var userGuess = "there is no user guess :("; // placeholder for a default user guess
+
+function floor1() {
+	currentFloorArray = floor1ListCrop;
+}
+
+function floor2() {
+	currentFloorArray = floor2ListCrop;
+}
+
+function floor3() {
+	currentFloorArray = floor3ListCrop;
+}
+
+function floor4() {
+	currentFloorArray = floor4ListCrop;
+}
+
+function floor5() {
+	currentFloorArray = floor5ListCrop;
+}
+
+var imagePath = "../../Images/"; // This is for the path of the images. in this case it is in the Images folder
+var jpg = ".jpg"; // This is for the extension of the images. in this case it is .jpg, just to to make it easier to add compare without the extension later
 
 function displayRandomImage() {
-	var randomIndex = Math.floor(Math.random() * imageList.length);
-	var randomImagePath = fullImagePath[randomIndex];
-	var imageElement = document.createElement("img");
-	imageElement.src = randomImagePath;
+	var fullImagePath = currentFloorArray.map(function (imageName) {
+		return imagePath + imageName + jpg; // This will return the full image path for each image in the array with the correct extension
+	});
+	var randomIndex = Math.floor(Math.random() * currentFloorArray.length); //Get random index number for a random image
+	var randomImagePath = fullImagePath[randomIndex]; //get random image path, this includes the path and the image name with current floor-array
+	var imageElement = document.createElement("img"); // Create an image element to put the image in
+	imageElement.src = randomImagePath; // This will put the image in the image element based on the random image path
 	imageElement.style.width = "300px"; // Set the width of the image
 	imageElement.style.height = "300px"; // Set the height of the image
-	var imageContainer = document.getElementById("Container1");
-	imageContainer.innerHTML = "";
-	imageContainer.appendChild(imageElement);
-
-	// Save random image chosen to use for blur
-	blurImage(randomImagePath);
-
-	window.alert("test work"); //test functionality
+	currentImage = currentFloorArray[randomIndex].toString().toUpperCase(); // Get the current image name as a string
+	console.log(currentImage);
+	var imageContainer = document.getElementById("Container1"); // Get the container into a variable
+	imageContainer.innerHTML = ""; // Clear the container
+	imageContainer.appendChild(imageElement); // Add the image to the container
 }
 
-//var currentPic
-//currentPic = "Hei"
-function martinFunctionTest() {
-	//var divContent = document.getElementById("testDiv");
-	divContent.innerHTML = '<img id="2.1" src="../Images/image2.jpg" alt="Image 2">';
+function checkGuess() {
+	userGuess = document.getElementById("guessInput").value.toUpperCase(); // Get the user guess from the guessInput field
+	var userGuessString = userGuess.toString().toUpperCase(); // Convert the user guess to a string and make it uppercase
+
+	if (userGuess + "-CROP" === currentImage) {
+		// check if the user guess is the same as the random image path (in an ettempt to be case-insensitive)
+		alert("Correct!");
+	} else {
+		alert("Wrong!");
+	}
 }
-
-function getRandImg() {
-	var randomIndexNum = Math.floor(Math.random() * imageList.length);
-	var randomImageName = imageList[randomIndexNum];
-	var imagePathMartin = "..images/" + randomImageName;
-
-	//alert(imagePathMartin);
-
-	divContent.innerHTML = "<img id = 'testId'>" + imagePathMartin + "<img>";
-
-	//var divContent = document.getElementById("testDiv");
-
-	//var imgElement = document.createElement("img");
-
-	//imageElement.src = imagePath;
-}
-
-
-function blurImage(chosen) {
-
-
-	document.getElementById("testId").style.filter = "blur(3Px)"; // trenger en måte å få element by object idk how
-
-}
-
-
-//takk for at du hjelper til <3
